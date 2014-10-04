28ed470a0c4ada4b14ced0efa961b2d0011689cd
Giovanni Maggini <giovanni.maggini@gmail.com>
2014-10-04 12:36:30 +0200

    cleaned up the updateTableOffset method

--- src/TapkuLibrary/TKCalendarMonthTableViewController.m
+++ src/TapkuLibrary/TKCalendarMonthTableViewController.m
@@ -86,17 +86,13 @@ static NSString *CellIdentifier = @"TKMonthDetailCell";
 		[UIView setAnimationDuration:0.4];
 		[UIView setAnimationDelay:0.1];
 	}
-
+	
 	
 	CGFloat y = CGRectGetMaxY(self.monthView.frame);
-	self.tableView.frame = CGRectMake(CGRectGetMinX(self.tableView.frame), y, CGRectGetWidth(self.tableView.frame) -self.topLayoutGuide.length, CGRectGetHeight(self.view.frame) - y);
+	self.tableView.frame = CGRectMake(CGRectGetMinX(self.tableView.frame), y, CGRectGetWidth(self.tableView.frame), CGRectGetHeight(self.view.frame) - y);
 	
-	if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7) {
-		//FIX for table view being shifted ~60px to the bottom. http://stackoverflow.com/questions/18388429/uitableview-is-starting-with-an-offset-in-ios-7
-		//This has something to do with iOS7+ automatically adjusting scrollview insets for table views
-		//Note: setting [self setAutomaticallyAdjustsScrollViewInsets:NO]; in loadView: does not work
-		self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
-	}
+	//The following method has been made necessary starting iOS7 onwards
+	self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
 	
 	if(animated) [UIView commitAnimations];
 }
